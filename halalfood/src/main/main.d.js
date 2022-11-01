8003b69bb562e8150d76fe5b35b25f90959397c8
R-serban <nurdavletkr@gmail.com>
2022-11-01 21:15:45 +0500

    begin the main page

---
+++ halalfood/src/main/main.d.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Main = void 0;
+var React = require("react");
+function Main() {
+    return (React.createElement("div", null,
+        React.createElement("h1", null, " Hello")));
+}
+exports.Main = Main;
---
+++ halalfood/src/main/main.d.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./main.css"
+export interface Main {
+
+}
+
+export function Main () {
+    return (
+        <>
+        <header>
+
+        </header>
+        </>
+    );
+}
